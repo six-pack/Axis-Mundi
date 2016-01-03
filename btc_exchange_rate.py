e2ac572fc698856fec85a965a6dfc9a129a6da10
Six-Pack <sixpack@sigaint.org>
2016-01-03 01:48:10 +0000

    Add support for multiple bitcoin exchange rate api sources  * Add automatic fail-over between api sources  * Current support for Bitpay and Bitcoincharts 24hr weighted

--- btc_exchange_rate.py
+++ btc_exchange_rate.py
@@ -26,18 +26,37 @@ class btc_exchange_rate(threading.Thread):
                    ('Accept-Language','en-us,en;q=0.5')]
         opener = urllib2.build_opener(SocksiPyHandler(socks.SOCKS5, self.socks_proxy, int(self.socks_port)))
         opener.addheaders = headers
+
+        BTC_API_BITPAY = 'http://bitpay.com/api/rates'
+        BTC_API_BITCOINCHARTS = 'http://api.bitcoincharts.com/v1/weighted_prices.json'
+        sources = [BTC_API_BITPAY,BTC_API_BITCOINCHARTS]
+        source_index = 0
         while self.running:
+            source = sources[source_index]# BTC_API_BITCOINCHARTS
             try:
-                response = opener.open('http://bitpay.com/api/rates',None,30).read()
-                print "Response" + str(response)
-            except:
-                print "Warning: Failed to retrieve current exchange rates via https/socks"
-                sleep (60) # try again in a minute TODO try other exchange api sources
-            else:
-                data = json.loads(response)
+                response = opener.open(source,None,30).read()
+                #print "Response" + str(response)
+                data_list = json.loads(response)
+                # Process based on source
+                if source == BTC_API_BITPAY:
+                    data = data_list # we can use these results as they come
+                elif source == BTC_API_BITCOINCHARTS:
+                    data = []
+                    for code in data_list:
+                        if not (code == 'CHF' or code == 'timestamp'): # For some reasson CHF is broken in the bitcoincharts api
+                            data.append({'code': code,'rate':data_list[code]['24h']})
+
                 task = queue_task(1, 'update_exchange_rates', data)
-                print "Exchange rates updated"
+                print "Exchange rates updated OK using " + source
                 self.queue.put(task)
                 sleep(900) # default 15 minutes TODO: Randomize this slightly (between 10 - 30 minutes)
+            except:
+                print "Warning: Failed to retrieve current exchange rates from " + source
+                if source_index < len(sources):
+                    source_index += 1
+                else:
+                    source_index = 0
+                sleep (10) # try again in 10 seconds
+
 
         print "Exchange rate collector shutting down"
