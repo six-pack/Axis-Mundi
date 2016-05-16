876b39651a5cf4c0174fd0cddf7bbca78633eb3c
Six-Pack <sixpack@sigaint.org>
2016-05-16 00:32:11 +0000

    Revert BTC exchange rate back to using urllib2 as dependencies are reduced. Set 'no_proxy' environ on OSX to prevent hard Python crash.

--- axismundi_client/btc_exchange_rate.py
+++ axismundi_client/btc_exchange_rate.py
@@ -1,33 +1,36 @@
 import threading
 from utilities import queue_task
+#import requests
+import urllib2
 import json
 from time import sleep
-import requests
+import socks
+from sockshandler import SocksiPyHandler
 import random
+import os
 
 class btc_exchange_rate(threading.Thread):
     # Thread to handle periodic exchange rate lookups. Queries multiple sources in case of failure
     # Requires a SOCKS proxy to be provided - by default the Tor SOCKS proxy is used
 
     def __init__(self, socks_proxy, socks_port, queue):
-        self.socks_proxy = str(socks_proxy)
-        self.socks_port = int(socks_port)
-        self.proxies_dict = {'http':'socks5://'+self.socks_proxy+':'+str(self.socks_port)}
+        self.socks_proxy = socks_proxy
+        self.socks_port = socks_port
         self.queue = queue
         self.running = True
         threading.Thread.__init__(self)
 
-
     def run(self):
-        print "Info: BTC Exchange rate thread started using SOCKS proxy " + self.socks_proxy + ":" + str(self.socks_port)
+        print "Info: BTC Exchange rate thread started using SOCKS proxy " + self.socks_proxy + ":" + self.socks_port
         # Make the request look like it came from a browser TODO - define the browser headers elsewhere so they can be easily updated
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0',
-                   'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                   'Accept-Language':'en-us,en;q=0.5'}
-        #opener = urllib2.build_opener(SocksiPyHandler(socks.SOCKS5, self.socks_proxy, int(self.socks_port)))
-        #opener.addheaders = headers
+        headers = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0'),
+                   ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                   ('Accept-Language','en-us,en;q=0.5')]
 
+        os.environ['no_proxy']='127.0.0.1'  # This needs to be set to avoid a hard crash on OSX when using urllib or requests
 
+        opener = urllib2.build_opener(SocksiPyHandler(socks.SOCKS5, self.socks_proxy, int(self.socks_port)))
+        opener.addheaders = headers
 
         BTC_API_BITPAY = 'http://bitpay.com/api/rates'
         BTC_API_BITCOINCHARTS = 'http://api.bitcoincharts.com/v1/weighted_prices.json'
@@ -37,8 +40,8 @@ class btc_exchange_rate(threading.Thread):
         while self.running:
             source = sources[source_index]# BTC_API_BITCOINCHARTS
             try:
-                #response = opener.open(source,None,30).read()
-                response = requests.get(source,headers=headers,proxies=self.proxies_dict).text
+                response = opener.open(source,None,30).read()
+                #print "Response" + str(response)
                 data_list = json.loads(response)
                 # Process based on source
                 if source == BTC_API_BITPAY:
