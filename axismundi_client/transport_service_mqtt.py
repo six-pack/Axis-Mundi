543987d551fdf980ab600d610d38191c996f49b0
El Presidente <El--Presidente@users.noreply.github.com>
2016-05-29 22:24:26 +0000

    Further enhancement of MQTT Message Transport Service.

--- axismundi_client/transport_service_mqtt.py
+++ axismundi_client/transport_service_mqtt.py
@@ -14,6 +14,7 @@ from collections import defaultdict
 import random
 import re
 import time
+import hashlib
 
 from mqtt_client import MQTT_ERR_SUCCESS, mqtt_cs_connected, MQTTMessage
 from mqtt_client import Client as mqtt
@@ -34,7 +35,9 @@ class Transport_Service_MQTT (threading.Thread):
 
         self.running = True
 
-        self.mqtt_queue_task_map = dict()
+        self.mqtt_mid_queue_task_map = dict()           # Dict holding mapping of MQTT MID to Task Queue ID
+        self.mqtt_sub_topic_queue_task_map = dict()     # Dict holding mapping of subscribed topics to task Queue ID, used for tracking subscribe/unsubscribing for transient subs such as key requests, profile requests etc
+        self.inbound_msg_hashes = set()                 # Set holding SHA1 hashes of messages received in this session - used only for naive de-duplication before passing message for verification and further processing
 
         self.tor_socks_host = tor_socks_host
         self.tor_socks_port = int(tor_socks_port)
@@ -61,6 +64,7 @@ class Transport_Service_MQTT (threading.Thread):
         self.transport.on_disconnect = self.on_disconnect
         self.transport.on_publish = self.on_publish
         self.transport.on_subscribe = self.on_subscribe
+        self.transport.on_unsubscribe = self.on_unsubscribe
 #        self.transport.message_callback_add('key/+', self.on_pgpkey) # TODO
         self.broker_connected = False
 
@@ -123,25 +127,43 @@ class Transport_Service_MQTT (threading.Thread):
 
 
     def on_message(self, client, userdata, msg):
-        logger.info('Message received %s using %s', self.current_broker, self.current_network)
+        logger.info('Message received on %s using %s', msg.topic, self.current_broker)
+        # Send incoming message to messaging system
+        hash = hashlib.sha1(msg.payload)
+        if hash.hexdigest() in self.inbound_msg_hashes:
+            logger.warning('Duplicate message on %s dropped', msg.topic)
+            return
+        else:
+            self.inbound_msg_hashes.add(hash.hexdigest())
+
+        queue_update_msg = queue_task(id='mts_mqtt:0',
+                                     command='inbound_message',
+                                     data={'payload':msg.payload,'location':msg.topic},
+                                     msg_type=queue_task.UPDATE)
+        self.out_queue.put(queue_update_msg)
 
 
     def on_disconnect(self, client, userdata, rc):
+        self.broker_connected = False
         if rc == 0:
             # This appears to be a graceful disconnect
             logger.info('Disconnected from broker %s using %s', self.current_broker, self.current_network)
         else:
             # This appears to be an unexpected disconnect
             logger.warning('Unexpectedly disconnected from broker %s using %s', self.current_broker, self.current_network)
-        self.broker_connected = False
+        # Flush any items in the queue task dicts, marking as NOT_OK and postingback REPLY messages
+        self.flush_unfinished_mqtt_operations()
+
+
 
 
     def on_publish(self, client, userdata, mid):
         logger.info('Message published sucessfuly on %s using %s', self.current_broker, self.current_network)
         try:
-            queue_msg_id = self.mqtt_queue_task_map[mid]
+            queue_msg_id = self.mqtt_mid_queue_task_map[mid]
+            self.mqtt_mid_queue_task_map.__delitem__(mid)
         except KeyError:
-            logger.error('Could not associate outbound MQTT message ID with a queue task id')
+            logger.error('Could not associate outbound MQTT message ID with a queue task ID')
             return
         # Delivered
         queue_reply_msg = queue_task(queue_msg_id,
@@ -153,8 +175,36 @@ class Transport_Service_MQTT (threading.Thread):
 
 
     def on_subscribe(self, client, userdata, mid, granted_qos):
-        logger.info('Topic Subscription on broker %s using %s', self.current_broker, self.current_network)
+        logger.info('Topic subscribed on broker %s using %s', self.current_broker, self.current_network)
+        try:
+            queue_msg_id = self.mqtt_mid_queue_task_map[mid]
+            subscribed_topic = self.mqtt_sub_topic_queue_task_map[queue_msg_id]
+            #logger.info('Now unsubscribing from %s',subscribed_topic)
+            u_rc,u_mid = self.transport.unsubscribe (subscribed_topic)
+            self.mqtt_mid_queue_task_map[u_mid] = queue_msg_id  # create a new mqtt MID <> queue mapping for the unsubscribe
+            self.mqtt_mid_queue_task_map.__delitem__(mid) # and delete the original queue mapping for the subscribe
 
+        except KeyError:
+            logger.info('Persistent subscription created')
+
+    def on_unsubscribe(self, client, userdata, mid):
+        logger.info('Topic unsubscribed on broker %s using %s', self.current_broker, self.current_network)
+        try:
+            queue_msg_id = self.mqtt_mid_queue_task_map[mid]
+            subscribed_topic = self.mqtt_sub_topic_queue_task_map[queue_msg_id]
+            logger.info('Now unsubscribed from %s (originally requested for %s)',subscribed_topic,queue_msg_id)
+            # TODO - if no message was received for this subscription by now then it is now safe to assume that no message exists - deal with that here or in messaging?
+            self.mqtt_mid_queue_task_map.__delitem__(mid)
+            self.mqtt_sub_topic_queue_task_map.__delitem__(queue_msg_id)
+            # get_message has completed sucessfully in any case
+            queue_reply_msg = queue_task(id=queue_msg_id,
+                                         command=None,
+                                         data=None,
+                                         rc=queue_task.OK,
+                                         msg_type=queue_task.REPLY)
+            self.out_queue.put(queue_reply_msg)
+        except KeyError:
+            logger.info('Unsubscribed from persistent connection (probably)')
 
 
     def on_pgpkey(self, client, userdata, msg):
@@ -164,18 +214,35 @@ class Transport_Service_MQTT (threading.Thread):
     def mqtt_publish_message (self, msg=None, mqtt_topic=None, qos=1, queue_task_id=None, publish=False):
 
         rc,mid = self.transport.publish(topic=mqtt_topic,payload=msg,qos=1,retain=publish)
-
         if not rc == MQTT_ERR_SUCCESS:
             # Not delivered
             queue_reply_msg = queue_task(id=queue_task_id,
-                                         command=None,
-                                         data=None,
                                          rc=queue_task.NOT_OK,
                                          msg_type=queue_task.REPLY)
             self.out_queue.put(queue_reply_msg)
-            logger.warning('Could not deliver MQTT message to to %s',mqtt_topic)
+            logger.warning('Could not deliver MQTT message to %s requested by task queue ID %s',mqtt_topic,queue_task_id)
         else:
-            self.mqtt_queue_task_map[mid]=queue_task_id  # Store the returned MID and associate it with the queue message id
+            self.mqtt_mid_queue_task_map[mid]=queue_task_id  # Store the returned MID and associate it with the queue message id
+
+
+
+    def mqtt_subscribe_message (self, mqtt_topic=None, qos=0, queue_task_id=None, persistent=False):
+
+        rc,mid = self.transport.subscribe(topic=mqtt_topic,qos=qos)
+
+        if not rc == MQTT_ERR_SUCCESS:
+            # Could not subscribe to get message
+            queue_reply_msg = queue_task(id=queue_task_id,
+                                         rc=queue_task.NOT_OK,
+                                         msg_type=queue_task.REPLY)
+            self.out_queue.put(queue_reply_msg)
+            logger.warning('Could not subscribe to MQTT topic %s requested by task queue ID %s',mqtt_topic,queue_task_id)
+        else:
+            if not persistent:
+                # Only store the mid if the request is for a single retained message - if a mid is present during on_subscribe() then we will unsubscribe
+                self.mqtt_mid_queue_task_map[mid] = queue_task_id               # Store the returned MID and associate it with the queue message id
+                self.mqtt_sub_topic_queue_task_map[queue_task_id] = mqtt_topic      # Store the desired topic associted withe the queue_task_id
+
 
 
     def check_queues(self):
@@ -207,17 +274,19 @@ class Transport_Service_MQTT (threading.Thread):
                 self.mqtt_publish_message (msg=msg, mqtt_topic=mqtt_topic, qos=1, queue_task_id=queue_msg.id)
 
             elif queue_msg.command == 'publish_msg':
-                mqtt_topic = 'mesh/local/user/' + queue_msg.data['location']    # location = ( key | direcory | profile | items )
+                mqtt_topic = 'mesh/local/user/' + self.pgp_key_id + '/' + queue_msg.data['location']    # location = ( key | direcory | profile | items )
                 msg = queue_msg.data['content']
                 self.mqtt_publish_message (msg=msg, mqtt_topic=mqtt_topic, qos=1, queue_task_id=queue_msg.id, publish=True)
 
-            elif queue_msg.command == 'publish_msg':
-                pass
+            elif queue_msg.command == 'get_published_msg':
+                mqtt_topic = 'mesh/+/user/' + queue_msg.data['location']    # location = ( key | direcory | profile | items )
+                self.mqtt_subscribe_message(mqtt_topic=mqtt_topic,qos=0,queue_task_id=queue_msg.id)
 
 
     def process_queued_task_reply(self,queue_msg):
         # The only replies the message transport service gets are authentication messages which are prepared by the messaging system for us
         if queue_msg.command == 'make_mqtt_pgp_auth' and queue_msg.rc == queue_task.OK:
+            logger.info('Received authentication ticket from messaging service for broker %s', self.current_broker)
             self.broker_password = queue_msg.data
 
 
@@ -229,6 +298,20 @@ class Transport_Service_MQTT (threading.Thread):
         self.out_queue.put (auth_req_task)
 
 
+    def flush_unfinished_mqtt_operations(self):
+        logger.info('Checking for incomplete MQTT tasks')
+        for mid,task_id in self.mqtt_mid_queue_task_map.items():
+            queue_reply_msg = queue_task(task_id,
+                             command=None,
+                             data=None,
+                             rc=queue_task.NOT_OK,
+                             msg_type=queue_task.REPLY)
+            self.out_queue.put(queue_reply_msg)
+            logger.warning('Failing and purging incomplete MQTT task %s',task_id)
+        self.mqtt_mid_queue_task_map.clear()
+        self.mqtt_sub_topic_queue_task_map.clear()
+        self.inbound_msg_hashes.clear()
+
     def run(self):
 
         connect_time = 0
@@ -236,9 +319,13 @@ class Transport_Service_MQTT (threading.Thread):
         while self.running:
             if not self.broker_connected:
 
-                if time.time() - connect_time > 5: # If more than X seconds since last connection attempt then we try another broker
+                if time.time() - connect_time > 10: # If more than X seconds since last connection attempt then we try another broker
+
+                    if not connect_time == 0:
+                        self.transport.loop_stop(force=True)
+                        self.flush_unfinished_mqtt_operations()
+
                     connect_time = time.time()
-                    self.transport.loop_stop(force=True)
                     self.select_random_broker()
 
                     logger.info('Attempting connection to broker %s using %s', self.current_broker, self.current_network)
@@ -253,7 +340,7 @@ class Transport_Service_MQTT (threading.Thread):
 
                     self.transport.username_pw_set(self.pgp_key_id, self.broker_password)
                     connect_time = time.time()
-                    self.transport.connect_async(self.current_broker,1883,keepalive=60)
+                    self.transport.connect_async(self.current_broker,1883,keepalive=30)
                     self.transport.loop_start()
 
             # Check queue for new tasks
@@ -261,7 +348,9 @@ class Transport_Service_MQTT (threading.Thread):
             sleep(0.01)
 
         logger.info('Stopping Message Transport Service (MQTT)')
-        self.transport.loop_stop()
+        self.transport._send_disconnect()   # This seems necessary but feels nasty - TODO - investigate
+        self.transport.loop_stop(force=True)
         self.transport.loop(timeout=1)
+        self.flush_unfinished_mqtt_operations()
         logger.info('Message Transport Service (MQTT) exiting')
 
